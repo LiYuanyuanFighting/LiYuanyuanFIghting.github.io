3c6734df3413499043f76b866dc117070eee0c02
LiYuanyuanFighting <zizimumu00@gmail.com>
2016-12-13 17:15:31 +0800

    Update javaProblem_assign4_Board.java

--- _posts/javaProblem_assign4_Board.java
+++ _posts/javaProblem_assign4_Board.java
@@ -46,13 +46,13 @@ public class Board {
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++) {
             // if the position is right, no need to count
-            if (blocks[i][j] == 3*i+j)
+            if (blocks[i][j] == (N * i + j + 1))
                 continue;
             // get the distance in the vertical direction
-            vertical = blocks[i][j]/N - i;
+            vertical = (blocks[i][j]-j-1)/N - i;
             vertical = Math.abs(vertical);
             // get the distance in the horizontal direction
-            horizontal = blocks[i][j] % N;
+            horizontal = (blocks[i][j]-1) % N-j;
             horizontal = Math.abs(horizontal);
             num += vertical + horizontal;
         }
@@ -134,99 +134,124 @@ public class Board {
         
         // 1st find the empth cell
         int ii = 0, jj = 0;
-        for (ii = 0; ii < N; ii++)
+        // record the position of empty cell
+       int row = 0;
+       int col = 0;
+        mainLoop:
+       for (ii = 0; ii < N; ii++) {
             for (jj = 0; jj < N; jj++) {
-            if (blocks[ii][jj]== 0)
-                break;
+            System.out.println("blocks[ii][jj] is " + blocks[ii][jj]);
+            if (blocks[ii][jj]== 0){
+                System.out.println("0");
+                break mainLoop;
+            }
         }
-       // record the position of empty cell
-       int row = ii;
-       int col = jj;
+       }
+         row = ii;
+         col = jj;
+        System.out.println("ii is " + ii + " jj is " + jj);
+       
        int exchange = 0;
-       int[][] blocksCopy = new int[N][N];
+       
        
         // find neighbors by moving all the neighbor cells into the empty cell
        
         // 1.check up
         if (row > 0) {
+            int[][] blocksCopy1 = new int[N][N];
             // the cell to be exchanged
+            System.out.println("row is " + row + " col is " + col);
             exchange = blocks[row-1][col];
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < N; j++) {
                 if (blocks[i][j] == exchange)
-                     blocksCopy[i][j] = 0;
+                     blocksCopy1[i][j] = 0;
                 else {
                     if (blocks[i][j] == 0)
-                    blocksCopy[i][j] = 0;
+                    blocksCopy1[i][j] = exchange;
                     else 
-                        blocksCopy[i][j] = blocks[i][j];
+                        blocksCopy1[i][j] = blocks[i][j];
                 }
             }
             // create the neighbor
-            Board neighbor1 = new Board(blocksCopy);
+            Board neighbor1 = new Board(blocksCopy1);
             // boards.add(neighbor1);
             neighbors.push(neighbor1);
+            System.out.println("I added up neighbor");
         }
         // 2.check bottom
-        if (row < N) {
+        if (row < N - 1) {
+            int[][] blocksCopy2 = new int[N][N];
             // the cell to be exchanged
             exchange = blocks[row+1][col];
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < N; j++) {
                 if (blocks[i][j] == exchange)
-                     blocksCopy[i][j] = 0;
+                     blocksCopy2[i][j] = 0;
                 else {
                     if (blocks[i][j] == 0)
-                    blocksCopy[i][j] = 0;
+                    blocksCopy2[i][j] = exchange;
                     else 
-                        blocksCopy[i][j] = blocks[i][j];
+                        blocksCopy2[i][j] = blocks[i][j];
                 }
             }
             // create the neighbor
-            Board neighbor2 = new Board(blocksCopy);
+            Board neighbor2 = new Board(blocksCopy2);
             // boards.add(neighbor1);
             neighbors.push(neighbor2);
+            System.out.println("added bottom neighbor " + neighbor2);
+             System.out.println("I added bottom neighbor");
         }
         // 3.check left
          if (col > 0) {
+             int[][] blocksCopy3 = new int[N][N];
             // the cell to be exchanged
             exchange = blocks[row][col-1];
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < N; j++) {
                 if (blocks[i][j] == exchange)
-                     blocksCopy[i][j] = 0;
+                     blocksCopy3[i][j] = 0;
                 else {
                     if (blocks[i][j] == 0)
-                    blocksCopy[i][j] = 0;
+                    blocksCopy3[i][j] = exchange;
                     else 
-                        blocksCopy[i][j] = blocks[i][j];
+                        blocksCopy3[i][j] = blocks[i][j];
                 }
             }
             // create the neighbor
-            Board neighbor3 = new Board(blocksCopy);
+            Board neighbor3 = new Board(blocksCopy3);
             // boards.add(neighbor1);
             neighbors.push(neighbor3);
+            System.out.println("added left neighbor " + neighbor3);
+             System.out.println("I added left neighbor");
         }
          // 4.check right
-         if (col > 0) {
+         if (col < N - 1) {
+             int[][] blocksCopy4 = new int[N][N];
             // the cell to be exchanged
             exchange = blocks[row][col+1];
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < N; j++) {
                 if (blocks[i][j] == exchange)
-                     blocksCopy[i][j] = 0;
+                     blocksCopy4[i][j] = 0;
                 else {
                     if (blocks[i][j] == 0)
-                    blocksCopy[i][j] = 0;
+                    blocksCopy4[i][j] = exchange;
                     else 
-                        blocksCopy[i][j] = blocks[i][j];
+                        blocksCopy4[i][j] = blocks[i][j];
                 }
             }
             // create the neighbor
-            Board neighbor4 = new Board(blocksCopy);
+            Board neighbor4 = new Board(blocksCopy4);
             // boards.add(neighbor1);
             neighbors.push(neighbor4);
+            System.out.println("added right neighbor " + neighbor4);
+             System.out.println("I added right neighbor");
         }
+         for (Board check: neighbors) {
+            System.out.println("2121212traverse neighbors " + check);
+            }
+         
          return neighbors;
     }
     public String toString()               // string representation of this board (in the output format specified below)
@@ -235,10 +260,11 @@ public class Board {
             throw new java.lang.NullPointerException();
         StringBuilder s = new StringBuilder();
         s.append(N + "\n");
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < N; j++) {
         for (int i = 0; i < N; i++)
            s.append(String.format("%2d ", blocks[i][j]));
         s.append("\n");
+        }
         return s.toString();
     }
     private void swap(int a, int b) {
