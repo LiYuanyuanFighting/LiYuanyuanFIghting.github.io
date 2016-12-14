1bb289ccb428ac0f5498fe80245a7bb711dfe02f
LiYuanyuanFighting <zizimumu00@gmail.com>
2016-12-15 06:10:46 +0800

    Update javaProblem_assign4_Board.java

--- _posts/javaProblem_assign4_Board.java
+++ _posts/javaProblem_assign4_Board.java
@@ -2,37 +2,40 @@
  *date:08/12/2016
  * goal:solve the 8-puzzle problem (and its natural generalizations) using the A* search algorithm
  */
-import java.lang.Math;
 // import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdRandom;
 import java.util.Stack;
 
 public class Board {
     
-    private final int N;
+    private final int n;
     private int[][] blocks; // should make a new array to keep original immutable or not?
-    public Board(int[][] blocks)           // construct a board from an n-by-n array of blocks
+    public Board(int[][] blo)           // construct a board from an n-by-n array of blocks
     {
-        if (blocks.length == 0)
+        if (blo.length == 0)
             throw new java.lang.NullPointerException();
-        this.N = blocks.length;
-        this.blocks = blocks;
+        this.n = blo.length;
+        blocks = new int[n][n];
+        // this.blocks = blocks;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+               this.blocks[i][j] = blo[i][j];
     }
                                            // (where blocks[i][j] = block in row i, column j)
     public int dimension()                 // board dimension n
     {
-        return N;
+        return n;
     }
     public int hamming()                   // number of blocks out of place
     {
         int num = 0;
         int currentNum = 0;
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
              // if the position is right, no need to count
             if (blocks[i][j] == 0)
                 continue;
-            currentNum = i*N + j + 1;
+            currentNum = i*n + j + 1;
             if (blocks[i][j] != currentNum)
                 num++;
         }
@@ -43,16 +46,25 @@ public class Board {
         int num = 0;
         int horizontal = 0;
         int vertical = 0;
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++) {
-            // if the position is right, no need to count
-            if (blocks[i][j] == (N * i + j + 1))
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+            if (blocks[i][j] == 0)
                 continue;
+            // if the position is right, no need to count
+            if (blocks[i][j] == (n * i + j + 1))
+                continue;
+            if (blocks[i][j] % n == 0) {
+                vertical = blocks[i][j]/n - 1 - i;
+                horizontal = n - 1 - j; 
+            } else {
+                vertical = blocks[i][j]/n - i;
+                horizontal = blocks[i][j] % n -1 - j;
+            }
             // get the distance in the vertical direction
-            vertical = (blocks[i][j]-j-1)/N - i;
+           // vertical = (blocks[i][j]-j-1)/n - i;
             vertical = Math.abs(vertical);
             // get the distance in the horizontal direction
-            horizontal = (blocks[i][j]-1) % N-j;
+            // horizontal = (blocks[i][j]-1) % n-j;
             horizontal = Math.abs(horizontal);
             num += vertical + horizontal;
         }
@@ -69,8 +81,8 @@ public class Board {
     {
         // any->random?
         int[][] blocksCopy = copy();
-        int row = StdRandom.uniform(1, N);
-        int col = StdRandom.uniform(1, N);
+        int row = StdRandom.uniform(1, n);
+        int col = StdRandom.uniform(1, n);
         int col1 = 0;
         int row1 = 0;
         if (blocks[row][col] == 0)
@@ -81,10 +93,10 @@ public class Board {
                 row += 1;
         }
         // get the other block
-        if (col >0) {
+        if (col > 0) {
             col1 = col - 1;
             row1 = row;
-            if (blocks[col1].equals(null))
+            if (blocks[row1][col1] == 0)
             {
                 if (row > 0)
                     row1 = row - 1;
@@ -116,14 +128,15 @@ public class Board {
         if (this == y)
             return true;
         if (y == null)
-            throw new java.lang.NullPointerException();
+            return false;
+           // throw new java.lang.NullPointerException();
         if (this.getClass() != y.getClass())
             return false;
         Board that = (Board) y;
-        if (that.dimension() != N) return false;
+        if (that.dimension() != n) return false;
         int[][] blocksThat = that.blocks;
-        for (int i = 0; i < N; i++) 
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++) {
             if (blocksThat[i][j] != blocks[i][j])
                 return false;
         }
@@ -140,9 +153,9 @@ public class Board {
        int row = 0;
        int col = 0;
         mainLoop:
-       for (ii = 0; ii < N; ii++) {
-            for (jj = 0; jj < N; jj++) {
-            if (blocks[ii][jj]== 0){
+       for (ii = 0; ii < n; ii++) {
+            for (jj = 0; jj < n; jj++) {
+            if (blocks[ii][jj] == 0) {
                 break mainLoop;
             }
         }
@@ -156,12 +169,12 @@ public class Board {
        
         // 1.check up
         if (row > 0) {
-            int[][] blocksCopy1 = new int[N][N];
+            int[][] blocksCopy1 = new int[n][n];
             // the cell to be exchanged
-            System.out.println("row is " + row + " col is " + col);
+           // System.out.println("row is " + row + " col is " + col);
             exchange = blocks[row-1][col];
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++) {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++) {
                 if (blocks[i][j] == exchange)
                      blocksCopy1[i][j] = 0;
                 else {
@@ -178,12 +191,12 @@ public class Board {
 //            System.out.println("I added up neighbor");
         }
         // 2.check bottom
-        if (row < N - 1) {
-            int[][] blocksCopy2 = new int[N][N];
+        if (row < n - 1) {
+            int[][] blocksCopy2 = new int[n][n];
             // the cell to be exchanged
             exchange = blocks[row+1][col];
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++) {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++) {
                 if (blocks[i][j] == exchange)
                      blocksCopy2[i][j] = 0;
                 else {
@@ -202,11 +215,11 @@ public class Board {
         }
         // 3.check left
          if (col > 0) {
-             int[][] blocksCopy3 = new int[N][N];
+             int[][] blocksCopy3 = new int[n][n];
             // the cell to be exchanged
             exchange = blocks[row][col-1];
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++) {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++) {
                 if (blocks[i][j] == exchange)
                      blocksCopy3[i][j] = 0;
                 else {
@@ -224,12 +237,12 @@ public class Board {
 //             System.out.println("I added left neighbor");
         }
          // 4.check right
-         if (col < N - 1) {
-             int[][] blocksCopy4 = new int[N][N];
+         if (col < n - 1) {
+             int[][] blocksCopy4 = new int[n][n];
             // the cell to be exchanged
             exchange = blocks[row][col+1];
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++) {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++) {
                 if (blocks[i][j] == exchange)
                      blocksCopy4[i][j] = 0;
                 else {
@@ -257,9 +270,9 @@ public class Board {
         if (blocks.length == 0)
             throw new java.lang.NullPointerException();
         StringBuilder s = new StringBuilder();
-        s.append(N + "\n");
-        for (int i = 0; i < N; i++){
-         for (int j = 0; j < N; j++)
+        s.append(n + "\n");
+        for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++)
            s.append(String.format("%2d ", blocks[i][j]));
         s.append("\n");
         }
@@ -272,14 +285,11 @@ public class Board {
         array[row2][col2] = temp;
     }
     private int[][] copy() {
-        int[][]  blocksCopy = new int[N][N];
-        for (int i = 0; i < N; i++) 
-            for (int j = 0; j < N; j++)
+        int[][]  blocksCopy = new int[n][n];
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++)
             blocksCopy[i][j] = blocks[i][j];
         return blocksCopy;
     }
-    public static void main(String[] args) // unit tests (not graded)
-    {
     
-    }
 }
