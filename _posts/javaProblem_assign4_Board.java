f45a622725b344a7c0627eab8e78e39c67c18301
LiYuanyuanFighting <zizimumu00@gmail.com>
2016-12-17 15:57:23 +0800

    Update javaProblem_assign4_Board.java

--- _posts/javaProblem_assign4_Board.java
+++ _posts/javaProblem_assign4_Board.java
@@ -3,23 +3,65 @@
  * goal:solve the 8-puzzle problem (and its natural generalizations) using the A* search algorithm
  */
 // import edu.princeton.cs.algs4.MinPQ;
-import edu.princeton.cs.algs4.StdRandom;
+// import edu.princeton.cs.algs4.StdRandom;
 import java.util.Stack;
 
 public class Board {
     
     private final int n;
-    private int[][] blocks; // should make a new array to keep original immutable or not?
+    private char[] blocks;
+    // private int[][] blocks; // should make a new array to keep original immutable or not?
+    private int hammingNum;
+    private int manhattanNum;
+    private int boardX, boardY;
     public Board(int[][] blo)           // construct a board from an n-by-n array of blocks
     {
         if (blo.length == 0)
             throw new java.lang.NullPointerException();
         this.n = blo.length;
-        blocks = new int[n][n];
+        blocks = new char[n*n];
         // this.blocks = blocks;
+        hammingNum = 0;
+        manhattanNum = 0;
+        int num = 0;
+        int num1 = 0;
+        int currentNum = 0;
+        int horizontal = 0;
+        int vertical = 0;
         for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-               this.blocks[i][j] = blo[i][j];
+            for (int j = 0; j < n; j++) {
+               this.blocks[i*n + j] = (char) blo[i][j];
+               // calculate hamming distance
+                    // if the position is right, no need to count
+                    if (blocks[i*n + j] != 0) {
+                        currentNum = i*n + j + 1;
+                        if (blocks[i*n + j] != currentNum)
+                        num++;
+                        // calculate manhattanNum
+                        // if the position is right, no need to count
+                        if (blocks[i*n + j] == (n * i + j + 1))
+                            continue;
+                        if (blocks[i*n + j] % n == 0) {
+                            vertical = blocks[i*n + j]/n - 1 - i;
+                            horizontal = n - 1 - j; 
+                        } else {
+                            vertical = blocks[i*n + j]/n - i;
+                            horizontal = blocks[i*n + j] % n -1 - j;
+                        }
+                        // get the distance in the vertical direction
+                        // vertical = (blocks[i][j]-j-1)/n - i;
+                        vertical = Math.abs(vertical);
+                        // get the distance in the horizontal direction
+                        // horizontal = (blocks[i][j]-1) % n-j;
+                        horizontal = Math.abs(horizontal);
+                        num1 += vertical + horizontal;
+                    } else {
+                        boardX = i;
+                        boardY = j;
+                    }
+                this.hammingNum = num;
+                this.manhattanNum = num1;
+        }
     }
                                            // (where blocks[i][j] = block in row i, column j)
     public int dimension()                 // board dimension n
@@ -28,99 +70,73 @@ public class Board {
     }
     public int hamming()                   // number of blocks out of place
     {
-        int num = 0;
-        int currentNum = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-             // if the position is right, no need to count
-            if (blocks[i][j] == 0)
-                continue;
-            currentNum = i*n + j + 1;
-            if (blocks[i][j] != currentNum)
-                num++;
-        }
-        return num;
+        return hammingNum;
     }
     public int manhattan()                 // sum of Manhattan distances between blocks and goal
-    {
-        int num = 0;
-        int horizontal = 0;
-        int vertical = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-            if (blocks[i][j] == 0)
-                continue;
-            // if the position is right, no need to count
-            if (blocks[i][j] == (n * i + j + 1))
-                continue;
-            if (blocks[i][j] % n == 0) {
-                vertical = blocks[i][j]/n - 1 - i;
-                horizontal = n - 1 - j; 
-            } else {
-                vertical = blocks[i][j]/n - i;
-                horizontal = blocks[i][j] % n -1 - j;
-            }
-            // get the distance in the vertical direction
-           // vertical = (blocks[i][j]-j-1)/n - i;
-            vertical = Math.abs(vertical);
-            // get the distance in the horizontal direction
-            // horizontal = (blocks[i][j]-1) % n-j;
-            horizontal = Math.abs(horizontal);
-            num += vertical + horizontal;
-        }
-        return num;
+    { 
+        return manhattanNum;
     }
     public boolean isGoal()                // is this board the goal board?
     {
-        int moves = hamming();
-        if (moves == 0)
-            return true;
-        return false;
+        return hammingNum == 0;
     }
     public Board twin()                    // a board that is obtained by exchanging any pair of blocks
     {
+        int[][] twin = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+            twin[i][j] = (int) blocks[(i * n) + j];
+        }
+        if ((twin[0][0] == 0) || (twin[0][1] == 0))
+            swap(twin, 1, 0, 1, 1);
+        else
+            swap(twin, 0, 0, 0, 1);
+        return new Board(twin);
         // any->random?
-        int[][] blocksCopy = copy();
-        int row = StdRandom.uniform(1, n);
-        int col = StdRandom.uniform(1, n);
-        int col1 = 0;
-        int row1 = 0;
-        if (blocks[row][col] == 0)
-        {
-            if (row > 0)
-                row -= 1;
-            else
-                row += 1;
-        }
-        // get the other block
-        if (col > 0) {
-            col1 = col - 1;
-            row1 = row;
-            if (blocks[row1][col1] == 0)
-            {
-                if (row > 0)
-                    row1 = row - 1;
-                else
-                    row1 = row + 1;
-                col1 = col;
-            }
-        } else {
-            col1 = col + 1;
-            row1 = row;
-            if (blocks[row1][col1] == 0)
-            {
-                if (row > 0)
-                    row1 = row - 1;
-                else
-                    row1 = row + 1;
-                col1 = col;
-            }
-        }
-       // System.out.println("Before swapping:" + blocksCopy[row][col]);
-        // swap
-        swap(blocksCopy, row, col, row1, col1);
-       // System.out.println("After swapping:" + blocksCopy[row][col]);
-        return new Board(blocksCopy);
+//        int[][] blocksCopy = new int[n][n];
+//        for (int i = 0; i < n; i++) 
+//            for (int j = 0; j < n; j++)
+//            blocksCopy[i][j] = (int) blocks[i*n + j];
+//        int row = StdRandom.uniform(1, n);
+//        int col = StdRandom.uniform(1, n);
+//        int col1 = 0;
+//        int row1 = 0;
+//        if (blocks[row*n + col] == 0)
+//        {
+//            if (row > 0)
+//                row -= 1;
+//            else
+//                row += 1;
+//        }
+//        // get the other block
+//        if (col > 0) {
+//            col1 = col - 1;
+//            row1 = row;
+//            if (blocks[row1*n + col1] == 0)
+//            {
+//                if (row > 0)
+//                    row1 = row - 1;
+//                else
+//                    row1 = row + 1;
+//                col1 = col;
+//            }
+//        } else {
+//            col1 = col + 1;
+//            row1 = row;
+//            if (blocks[row1*n + col1] == 0)
+//            {
+//                if (row > 0)
+//                    row1 = row - 1;
+//                else
+//                    row1 = row + 1;
+//                col1 = col;
+//            }
+//        }
+//       // System.out.println("Before swapping:" + blocksCopy[row][col]);
+//        // swap
+//        swap(blocksCopy, row, col, row1, col1);
+//       // System.out.println("After swapping:" + blocksCopy[row][col]);
+//        return new Board(blocksCopy);
     }
     public boolean equals(Object y)        // does this board equal y?
     {
@@ -134,10 +150,9 @@ public class Board {
             return false;
         Board that = (Board) y;
         if (that.dimension() != n) return false;
-        int[][] blocksThat = that.blocks;
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < n; j++) {
-            if (blocksThat[i][j] != blocks[i][j])
+        char[] blocksThat = that.blocks;
+         for (int j = 0; j < n*n; j++) {
+            if (blocksThat[j] != blocks[j])
                 return false;
         }
         return true;
@@ -146,123 +161,31 @@ public class Board {
     public Iterable<Board> neighbors()     // all neighboring boards, take care not to change original blocks
     {
         Stack<Board> neighbors = new Stack<Board>();
-        
-        // 1st find the empth cell
-        int ii = 0, jj = 0;
-        // record the position of empty cell
-       int row = 0;
-       int col = 0;
-        mainLoop:
-       for (ii = 0; ii < n; ii++) {
-            for (jj = 0; jj < n; jj++) {
-            if (blocks[ii][jj] == 0) {
-                break mainLoop;
-            }
-        }
-       }
-         row = ii;
-         col = jj;
-       int exchange = 0;
-       
-       
         // find neighbors by moving all the neighbor cells into the empty cell
-       
-        // 1.check up
-        if (row > 0) {
-            int[][] blocksCopy1 = new int[n][n];
+       int[] dX = {0, 0, -1, 1};
+       int[] dY = {1, -1, 0, 0};
+       int[][] tmp = new int[n][n];
+       for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++)
+            tmp[i][j] = (int) blocks[i*n + j];
             // the cell to be exchanged
            // System.out.println("row is " + row + " col is " + col);
-            exchange = blocks[row-1][col];
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++) {
-                if (blocks[i][j] == exchange)
-                     blocksCopy1[i][j] = 0;
-                else {
-                    if (blocks[i][j] == 0)
-                    blocksCopy1[i][j] = exchange;
-                    else 
-                        blocksCopy1[i][j] = blocks[i][j];
-                }
+            
+                for (int j = 0; j < 4; j++) {
+                    int x = boardX + dX[j];
+                    int y = boardY + dY[j];
+                    if (x >= 0 && x < n && y >= 0 && y < n) {
+                    tmp[boardX][boardY] = tmp[x][ y];
+                    tmp[x][y] = 0;
+                    // create the neighbor
+                    Board neighbor1 = new Board(tmp);
+                    // boards.add(neighbor1);
+                    neighbors.push(neighbor1);
+                    // remember to recover the blocks 
+                    tmp[x][y] = tmp[boardX][boardY];
+                    tmp[boardX][boardY] = 0;
+                    }
             }
-            // create the neighbor
-            Board neighbor1 = new Board(blocksCopy1);
-            // boards.add(neighbor1);
-            neighbors.push(neighbor1);
-//            System.out.println("I added up neighbor");
-        }
-        // 2.check bottom
-        if (row < n - 1) {
-            int[][] blocksCopy2 = new int[n][n];
-            // the cell to be exchanged
-            exchange = blocks[row+1][col];
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++) {
-                if (blocks[i][j] == exchange)
-                     blocksCopy2[i][j] = 0;
-                else {
-                    if (blocks[i][j] == 0)
-                    blocksCopy2[i][j] = exchange;
-                    else 
-                        blocksCopy2[i][j] = blocks[i][j];
-                }
-            }
-            // create the neighbor
-            Board neighbor2 = new Board(blocksCopy2);
-            // boards.add(neighbor1);
-            neighbors.push(neighbor2);
-//            System.out.println("added bottom neighbor " + neighbor2);
-//             System.out.println("I added bottom neighbor");
-        }
-        // 3.check left
-         if (col > 0) {
-             int[][] blocksCopy3 = new int[n][n];
-            // the cell to be exchanged
-            exchange = blocks[row][col-1];
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++) {
-                if (blocks[i][j] == exchange)
-                     blocksCopy3[i][j] = 0;
-                else {
-                    if (blocks[i][j] == 0)
-                    blocksCopy3[i][j] = exchange;
-                    else 
-                        blocksCopy3[i][j] = blocks[i][j];
-                }
-            }
-            // create the neighbor
-            Board neighbor3 = new Board(blocksCopy3);
-            // boards.add(neighbor1);
-            neighbors.push(neighbor3);
-//            System.out.println("added left neighbor " + neighbor3);
-//             System.out.println("I added left neighbor");
-        }
-         // 4.check right
-         if (col < n - 1) {
-             int[][] blocksCopy4 = new int[n][n];
-            // the cell to be exchanged
-            exchange = blocks[row][col+1];
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++) {
-                if (blocks[i][j] == exchange)
-                     blocksCopy4[i][j] = 0;
-                else {
-                    if (blocks[i][j] == 0)
-                    blocksCopy4[i][j] = exchange;
-                    else 
-                        blocksCopy4[i][j] = blocks[i][j];
-                }
-            }
-            // create the neighbor
-            Board neighbor4 = new Board(blocksCopy4);
-            // boards.add(neighbor1);
-            neighbors.push(neighbor4);
-//            System.out.println("added right neighbor " + neighbor4);
-//             System.out.println("I added right neighbor");
-        }
-//         for (Board check: neighbors) {
-//            System.out.println("2121212traverse neighbors " + check);
-//            }
-         
          return neighbors;
     }
     public String toString()               // string representation of this board (in the output format specified below)
@@ -273,7 +196,7 @@ public class Board {
         s.append(n + "\n");
         for (int i = 0; i < n; i++) {
          for (int j = 0; j < n; j++)
-           s.append(String.format("%2d ", blocks[i][j]));
+           s.append(String.format("%2d ", (int) blocks[i*n + j]));
         s.append("\n");
         }
         return s.toString();
@@ -283,13 +206,6 @@ public class Board {
         temp = array[row1][col1];
         array[row1][col1] = array[row2][col2];
         array[row2][col2] = temp;
-    }
-    private int[][] copy() {
-        int[][]  blocksCopy = new int[n][n];
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < n; j++)
-            blocksCopy[i][j] = blocks[i][j];
-        return blocksCopy;
     }
     
 }
